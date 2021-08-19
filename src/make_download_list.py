63be803b8e8a5d819aef19733b77b0f7e9a4f32d
John F. Wu <jwuphysics@gmail.com>
2021-08-19 09:46:45 -0400

    vary pixel scale (resolution)

--- src/make_download_list.py
+++ src/make_download_list.py
@@ -3,6 +3,7 @@ from pathlib import Path
 PATH = Path(__file__).resolve().parent.parent
 
 layer = 'sdss' # vs ls-dr9
+pixscale = 0.262
 size = 160
 
 df = pd.read_csv(PATH/'data/agn_cleaned.csv')
@@ -12,4 +13,4 @@ with open(PATH/'data/image_urls.txt', 'a') as f:
         objID = row.objID
         ra, dec = row.ra, row.dec
 
-        f.write(f'{objID}.jpg http://legacysurvey.org/viewer/cutout.jpg?ra={ra}&dec={dec}&pixscale=0.262&layer={layer}&size={size}\n')
+        f.write(f'{objID}.jpg http://legacysurvey.org/viewer/cutout.jpg?ra={ra}&dec={dec}&pixscale={pixscale}&layer={layer}&size={size}\n')
